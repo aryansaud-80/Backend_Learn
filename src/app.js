af4c9ee4c68743e0a1d1ff06495316c1b07e5731
aryansaud-80 <aryansaud080+github@gmail.com>
2024-12-08 22:07:27 +0545

    Fixes bug in app.js file

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ dotenv.config({ path: './.env' });
 
 const app = express();
 
+
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
